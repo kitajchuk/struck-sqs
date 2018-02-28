ac54e05db6ce87c5d1ac9f21e1353772abc1ece8
kitajchuk <kitajchuk@gmail.com>
2018-02-28 11:08:11 -0800

    FPO tout for related items module

--- source/js/views/related.js
+++ source/js/views/related.js
@@ -34,5 +34,19 @@ export default ( json, data ) => {
                 <img class="media__node js-lazy-image -wrapl" data-img-src="${image.assetUrl}" />
             </div>
         </div>
-    ` : ``;
+
+    ` : `
+        <div class="related__info -column -vtop">
+            <div class="m -caps">This is not real</div>
+            <div class="h4 -fmedium">Seriously, this is not supposed to be here.</div>
+            <div class="-bump3">
+                <a class="m -color -plusline" href="https://giphy.com/gifs/skateboarding-skateboard-kick-flip-3oEdv5FXteGY8iS8CY" target="_blank">+ <span>But check this out</span></a>
+            </div>
+        </div>
+        <div class="related__image -column -vtop">
+            <div class="media -wrapl">
+                <img class="related__fpo media__node js-lazy-image -wrapl" data-img-src="https://static1.squarespace.com/static/58e7dfe8bf629ae56bff881b/t/5a96fb2c24a694718d39eaf3/1519844145290/giphy-downsized.gif?format=original" />
+            </div>
+        </div>
+    `;
 };
