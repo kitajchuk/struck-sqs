d0e54e739620ba4f79df1a36991848d0af440163
kitajchuk <kitajchuk@gmail.com>
2018-03-16 17:42:05 -0700

    intro animation sequence

--- source/js/intro.js
+++ source/js/intro.js
@@ -10,16 +10,47 @@ import * as core from "./core";
  */
 const intro = {
     exec () {
-        return new Promise(( resolve ) => {
-            setTimeout(() => {
-                core.dom.intro.removeClass( "is-active" );
+        this.duration = 350;
+        this.delay = 80;
+        this.ids = [
+            { id: "love", el: core.dom.intro.find( ".love" ) },
+            { id: "fight", el: core.dom.intro.find( ".fight" ) },
+            { id: "adapt", el: core.dom.intro.find( ".adapt" ) }
+        ];
 
-                resolve();
-
-                core.emitter.fire( "app--intro-teardown" );
-
-            }, 2000 );
+        return new Promise(( resolve, reject ) => {
+            this.resolve = resolve;
+            this.reject = reject;
+            this.anim();
         });
+    },
+
+
+    anim () {
+        const obj = this.ids.shift();
+        const len = obj.el.find( "path" ).length;
+
+        obj.el.addClass( "is-animated" );
+
+        setTimeout(() => {
+            if ( !this.ids.length ) {
+                this.done();
+
+            } else {
+                this.anim();
+            }
+
+        }, (this.duration + (this.delay * len) + this.duration) );
+    },
+
+
+    done () {
+        setTimeout(() => {
+            core.emitter.fire( "app--intro-teardown" );
+            core.dom.intro.addClass( "is-animated" );
+            this.resolve();
+
+        }, this.duration * 4 );
     }
 };
 
