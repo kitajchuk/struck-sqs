323b26998bd0d70c8e28afda316771ed809770e8
kitajchuk <kitajchuk@gmail.com>
2018-06-01 11:16:55 -0700

    remove spotColor and show GIF on hover for grid tiles

--- source/js/views/grid.js
+++ source/js/views/grid.js
@@ -4,27 +4,26 @@ export default ( json/*, data*/ ) => {
     return items.map(( item ) => {
         const classMod = (item.customContent.customType === "customProject" ? "4up" : "2up");
         const postType = (item.customContent.customType === "customProject" ? "Project" : "Case Study");
-        const gridImage = (item.customContent.animatedGif ? item.customContent.animatedGif : item);
         const aspectRatio = (item.customContent.customType === "customProject" ? "415x492" : "850x492");
         const eventAction = (item.customContent.customType === "customProject" ? "portfolio" : "case-studies");
+        const gifClass = (item.customContent.animatedGif ? "grid__item--gif" : "");
         const tagsHtml = item.tags ? item.tags.map(( tag ) => {
             return `<div class="filters__item p -light -column">${tag}</div>`;
 
         }).join( "" ) : "";
 
         return item.starred ? `
-            <a class="grid__item grid__item--${classMod} -column js-gtm-event" data-ec="site interaction" data-ea="${eventAction}" data-el="${item.title}" href="${item.fullUrl}">
+            <a class="grid__item ${gifClass} grid__item--${classMod} -column js-gtm-event" data-ec="site interaction" data-ea="${eventAction}" data-el="${item.title}" href="${item.fullUrl}">
                 <div class="grid__item-wrap anim anim--tr js-lazy-anim">
-                    <div class="grid__image js-lazy-image js-aspect -cover" data-img-src="${gridImage.assetUrl}" data-variants="${gridImage.systemDataVariants}" data-original="${aspectRatio}"></div>
-                    ${item.customContent.spotColor ? `
-                        <div class="grid__color" style="background-color:${item.customContent.spotColor};"></div>
-                    ` : `
-                        <div class="grid__hover">
-                            <div class="grid__desc cms -light">
-                                ${item.excerpt}
-                            </div>
+                    <div class="grid__image js-lazy-image js-aspect -cover" data-img-src="${item.assetUrl}" data-variants="${item.systemDataVariants}" data-original="${aspectRatio}"></div>
+                    <div class="grid__hover">
+                        ${item.customContent.animatedGif ? `
+                            <div class="grid__gif js-lazy-image js-aspect -cover" data-img-src="${item.customContent.animatedGif.assetUrl}" data-variants="${item.customContent.animatedGif.systemDataVariants}" data-original="${aspectRatio}"></div>
+                        ` : ``}
+                        <div class="grid__desc cms -light">
+                            ${item.excerpt}
                         </div>
-                    `}
+                    </div>
                     <div class="grid__info">
                         <div class="grid__title m -light -caps">${item.title}</div>
                         <div class="grid__meta">
